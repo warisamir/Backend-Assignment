2240b2d15347d09684a6a2cb80472c851f1c9009
warisamir <warisamir1918@gmail.com>
2024-04-17 08:44:01 +0530

    nothing clena

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 const express=require("express")
 require("dotenv").config()
-<<<<<<< HEAD
+
 const connection=require("./connection/db");
 const userRoute=require("./route/userRoute")
 const productRoute=require("./route/productRoute")
 const cartRoute=require("./route/cartRoute")
-=======
-const connection=require("./Connection/db");
-const userRoute=require("./Route/userRoute")
-const productRoute=require("./Route/ProductRoute")
-const cartRoute=require("./Route/cartRoute")
->>>>>>> 1cf3d2e2665e318c652da958024aea88fddcd07c
+
 const auth=require("./middleware/auth")
 const app=express()
 app.use(express.json())
